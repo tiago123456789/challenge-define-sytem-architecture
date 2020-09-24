4d47e9876500ffdc03290534513ef591117b77f3
Tiago Rosa da costa <tiagorosadacost@gmail.com>
2020-09-24 20:50:01 -0300

    Added rate limit middleware

--- api-gateway/src/config/Server.js
+++ api-gateway/src/config/Server.js
@@ -3,11 +3,14 @@ const helmet = require("helmet");
 const app = express();
 require("dotenv").config();
 const routesApp = require("../routes");
+const rateLimitMiddleware = require("common/middlewares/RateLimitMiddleware");
 
 // Enable helmet.
-app.use(helmet());
+app.use(helmet()); 
 
+// Enable rate limit middleware.
+app.use(rateLimitMiddleware); 
 
 routesApp(app);
 
-module.exports = app;  
+module.exports = app;   
